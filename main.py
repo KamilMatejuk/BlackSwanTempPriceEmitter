c4fd2b0f9460e3acb34fcce0a1e8c3c207391a84
Kamil Matejuk <kamilmatejuk@gmail.com>
2023-10-14 13:46:34 +0200

    indicators

--- main.py
+++ main.py
@@ -5,23 +5,62 @@ from decouple import Config, RepositoryEnv
 from flask_socketio import SocketIO
 
 
-def get_price_from_timerange(
+def _validate_tokenpair(tokenPair: str):
+    assert tokenPair == "BTCUSDT", "Temporarly only BTCUSDT token pair is supported"
+
+def _validate_interval(interval: str):
+    assert interval == "1d", "Temporarly only 1d interval is supported"
+
+def _validate_times(startTime: int, endTime: int):
+    assert startTime >= 1503100799999, "Temporarly cannot start before 18.08.2017 (Unix 1503100799999)"
+    assert endTime <= 1693180799999, "Temporarly cannot end after 27.08.2023 (Unix 1693180799999)"
+    assert startTime < endTime, "Start time has to be before end time"
+
+def _validate_indicator(indicator: list, columns: list):
+    assert indicator in columns, f"Unknown indicator {indicator}, only available are {columns}"
+
+
+def get_price_for_timerange(
         tokenPair: str,
         interval: str,
         startTime: int,
         endTime: int):
     try:
-        assert tokenPair == "BTCUSDT", "Temporarly only BTCUSDT token pair is supported"
-        assert interval == "1d", "Temporarly only 1d interval is supported"
-        assert startTime >= 1503100799999, "Temporarly cannot start before 18.08.2017 (Unix 1503100799999)"
-        assert endTime <= 1693180799999, "Temporarly cannot end after 27.08.2023 (Unix 1693180799999)"
-        assert startTime < endTime, "Start time has to be before end time"
+        _validate_tokenpair(tokenPair)
+        _validate_interval(interval)
+        _validate_times(startTime, endTime)
     except Exception as ex:
         return jsonify({"error": "Invalid request schema", "details": str(ex)}), 401
     
     data = pd.read_csv(f'data/binance_{tokenPair}_{interval}.csv')
     data = data[['timestamp_close', 'price_close']]
     data = data.rename(columns={'timestamp_close': 'timestamp', 'price_close': 'price'})
+    data = data[(data['timestamp'] >= startTime) & (data['timestamp'] <= endTime)]
+    return list(data.T.to_dict().values()), 200
+
+
+def get_indicators_for_timerange(
+        tokenPair: str,
+        interval: str,
+        startTime: int,
+        endTime: int,
+        indicator: str):
+    try:
+        _validate_tokenpair(tokenPair)
+        _validate_interval(interval)
+        _validate_times(startTime, endTime)
+    except Exception as ex:
+        return jsonify({"error": "Invalid request schema", "details": str(ex)}), 401
+
+    data = pd.read_csv(f'data/binance_{tokenPair}_{interval}.csv')
+    
+    try:
+        _validate_indicator(indicator, list(data.columns))
+    except Exception as ex:
+        return jsonify({"error": "Invalid request schema", "details": str(ex)}), 401
+    
+    data = data[['timestamp_close', indicator]]
+    data = data.rename(columns={'timestamp_close': 'timestamp'})
     data = data[(data['timestamp'] >= startTime) & (data['timestamp'] <= endTime)]
     return list(data.T.to_dict().values()), 200
 
